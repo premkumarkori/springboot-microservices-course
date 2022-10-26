ff3283f55087ef8ce3a9b81a0db2c4f57d787789
premkumarkori <premkumar.kori@mavs.uta.edu>
2022-10-25 20:31:39 -0500

    Added Eureka Server

--- ProductService/src/main/java/com/premkumar/ProductService/ProductServiceApplication.java
+++ ProductService/src/main/java/com/premkumar/ProductService/ProductServiceApplication.java
@@ -3,10 +3,12 @@ package com.premkumar.ProductService;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
 @OpenAPIDefinition
+@EnableEurekaClient
 public class ProductServiceApplication {
 
     public static void main(String[] args) {
