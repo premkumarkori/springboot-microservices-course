920eac9431b33974821f23c39b366c52d1380c73
premkumarkori <premkumar.kori@mavs.uta.edu>
2022-10-29 22:32:01 -0500

    Implemented APIGW,CircuitBreaker,RateLimiter in GW using Resilience 4j & Redis

--- Clients/src/main/java/com/premkumar/clients/paymentservice/PaymentClient.java
+++ Clients/src/main/java/com/premkumar/clients/paymentservice/PaymentClient.java
@@ -1,6 +1,8 @@
 package com.premkumar.clients.paymentservice;
 
 import com.premkumar.clients.config.FeignConfig;
+import com.premkumar.clients.exception.CustomException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+@CircuitBreaker(name = "external", fallbackMethod = "fallback")
 @FeignClient(name = "paymentservice/payments", configuration = FeignConfig.class)
 public interface PaymentClient {
 
@@ -16,4 +19,11 @@ public interface PaymentClient {
 
     @GetMapping("/order/{orderId}")
     ResponseEntity<PaymentResponse> getOrderDetailsByOrderId(@PathVariable("orderId") String orderId);
+
+    default ResponseEntity<Long> fallback(Exception e) {
+        throw new CustomException("Payment Service is not available",
+                "UNAVAILABLE",
+                500);
+    }
 }
+
--- Clients/src/main/java/com/premkumar/clients/productservice/ProductClient.java
+++ Clients/src/main/java/com/premkumar/clients/productservice/ProductClient.java
@@ -1,6 +1,8 @@
 package com.premkumar.clients.productservice;
 
 import com.premkumar.clients.config.FeignConfig;
+import com.premkumar.clients.exception.CustomException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@CircuitBreaker(name = "external", fallbackMethod = "fallback")
 @FeignClient(name = "productservice/products", configuration = FeignConfig.class)
 public interface ProductClient {
 
@@ -20,4 +23,10 @@ public interface ProductClient {
 
     @GetMapping("/{id}")
     ResponseEntity<ProductResponse> getProductById(@PathVariable("id") long productId);
+
+    default ResponseEntity<Long> fallback(Exception e) {
+        throw new CustomException("Product Service is not available",
+                "UNAVAILABLE",
+                500);
+    }
 }
